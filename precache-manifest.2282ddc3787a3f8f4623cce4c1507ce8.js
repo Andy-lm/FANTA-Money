7d3606487789bfe789b43deecabc87a34e3aef8a
LM <1348189027@qq.com>
2021-03-14 23:00:52 +0800

    deploy

---
+++ precache-manifest.2282ddc3787a3f8f4623cce4c1507ce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f28e6b9031747bfa4b",
+    "url": "/FANTA-Money/css/app.8550afdb.css"
+  },
+  {
+    "revision": "bd4bc4a9191df316e1ccd0d3c1170337",
+    "url": "/FANTA-Money/index.html"
+  },
+  {
+    "revision": "c5f28e6b9031747bfa4b",
+    "url": "/FANTA-Money/js/app.6a4c23be.js"
+  },
+  {
+    "revision": "86a5fb89d464c479ad9a",
+    "url": "/FANTA-Money/js/chunk-vendors.8b57b9cb.js"
+  },
+  {
+    "revision": "87caf51c64178e1840b64b57110189f8",
+    "url": "/FANTA-Money/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/FANTA-Money/robots.txt"
+  }
+]);
